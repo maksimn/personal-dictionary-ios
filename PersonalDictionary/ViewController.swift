133094ddeb66eefd7a622eb4a9f16c54c5e7f8af
maksimn <maksim1989sovunion@yandex.ru>
2021-09-30 00:57:31 +0300

    Начальный проект.

---
+++ PersonalDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalDictionary
+//
+//  Created by Maxim Ivanov on 30.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
