450bd689170f649de620a11bb0a7c676babcdf65
maksimn <maksim1989sovunion@yandex.ru>
2021-10-05 11:50:17 +0300

    [TZ-1]: App Localization (Eng, Ru).

--- PersonalDictionary/Resources.swift
+++ PersonalDictionary/Resources.swift
@@ -7,17 +7,17 @@
 
 import UIKit
 
-let langResourceData = LangResourceData(allLangs: [Lang(name: "Английский"),
-                                                   Lang(name: "Русский"),
-                                                   Lang(name: "Французский"),
-                                                   Lang(name: "Итальянский")],
+let langResourceData = LangResourceData(allLangs: [Lang(name: NSLocalizedString("English", comment: "")),
+                                                   Lang(name: NSLocalizedString("Russian", comment: "")),
+                                                   Lang(name: NSLocalizedString("French", comment: "")),
+                                                   Lang(name: NSLocalizedString("Italian", comment: ""))],
                                         sourceLangKey: "io.github.maksimn.pd.sourceLang",
                                         targetLangKey: "io.github.maksimn.pd.targetLang",
-                                        defaultSourceLang: Lang(name: "Английский"),
-                                        defaultTargetLang: Lang(name: "Русский"))
+                                        defaultSourceLang: Lang(name: NSLocalizedString("English", comment: "")),
+                                        defaultTargetLang: Lang(name: NSLocalizedString("Russian", comment: "")))
 
-let newWordViewResource = NewWordViewResource(selectButtonTitle: "Выбрать",
-                                              arrowText: "⇋",
-                                              okText: "OK",
-                                              textFieldPlaceholder: "Впишите новое слово",
+let newWordViewResource = NewWordViewResource(selectButtonTitle: NSLocalizedString("Select", comment: ""),
+                                              arrowText: NSLocalizedString("⇋", comment: ""),
+                                              okText: NSLocalizedString("OK", comment: ""),
+                                              textFieldPlaceholder: NSLocalizedString("Enter a new word", comment: ""),
                                               backgroundColor: UIColor(red: 0.97, green: 0.97, blue: 0.95, alpha: 1.0))
