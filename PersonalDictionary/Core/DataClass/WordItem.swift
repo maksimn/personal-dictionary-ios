a7650c0dfd18a0d708d6c548c258b58f49f1599d
maksimn <maksim1989sovunion@yandex.ru>
2021-10-06 16:26:47 +0300

    [TZ-2]: WordItem.createdAt property.

--- PersonalDictionary/Core/DataClass/WordItem.swift
+++ PersonalDictionary/Core/DataClass/WordItem.swift
@@ -15,14 +15,17 @@ struct WordItem {
     let text: String
     let sourceLang: Lang
     let targetLang: Lang
+    let createdAt: Int
 
     init(id: Identifier = Identifier(rawValue: UUID().uuidString),
          text: String,
          sourceLang: Lang,
-         targetLang: Lang) {
+         targetLang: Lang,
+         createdAt: Int = Int(Date().timeIntervalSince1970)) {
         self.id = id
         self.text = text
         self.sourceLang = sourceLang
         self.targetLang = targetLang
+        self.createdAt = createdAt
     }
 }
