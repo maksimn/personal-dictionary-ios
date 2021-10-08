3ff6e13cedabfd61ccfe4b286246569d17471a2b
maksimn <maksim1989sovunion@yandex.ru>
2021-10-09 02:38:54 +0300

    [TZ-3]: Yandex Translator API key.

--- PersonalDictionary/Resources.swift
+++ PersonalDictionary/Resources.swift
@@ -23,3 +23,11 @@ let newWordViewResource = NewWordViewResource(selectButtonTitle: NSLocalizedStri
                                               okText: NSLocalizedString("OK", comment: ""),
                                               textFieldPlaceholder: NSLocalizedString("Enter a new word", comment: ""),
                                               backgroundColor: UIColor(red: 0.97, green: 0.97, blue: 0.95, alpha: 1.0))
+
+struct WebApiData {
+    let yandexTranslatorApiKey: String
+}
+
+let webApiData = WebApiData(
+    yandexTranslatorApiKey: "trnsl.1.1.20190308T225103Z.03f7eccfa6946358.fcc85b6387d6223f20a548dbcc0789a46272150b"
+)
