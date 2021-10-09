24f3cfe8f5965df2bc5e43ab6f3e0a688be4396b
maksimn <maksim1989sovunion@yandex.ru>
2021-10-09 23:36:31 +0300

    [TZ-3]: WordItem.translation property.

--- PersonalDictionary/Core/DataClass/WordItem.swift
+++ PersonalDictionary/Core/DataClass/WordItem.swift
@@ -13,19 +13,31 @@ struct WordItem {
 
     let id: Id
     let text: String
+    let translation: String?
     let sourceLang: Lang
     let targetLang: Lang
     let createdAt: Int
 
     init(id: Id = Id(raw: UUID().uuidString),
          text: String,
+         translation: String? = nil,
          sourceLang: Lang,
          targetLang: Lang,
          createdAt: Int = Int(Date().timeIntervalSince1970)) {
         self.id = id
         self.text = text
+        self.translation = translation
         self.sourceLang = sourceLang
         self.targetLang = targetLang
         self.createdAt = createdAt
+    }
+
+    func update(translation: String) -> WordItem {
+        WordItem(id: id,
+                 text: text,
+                 translation: translation,
+                 sourceLang: sourceLang,
+                 targetLang: targetLang,
+                 createdAt: createdAt)
     }
 }
