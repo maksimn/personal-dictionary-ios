d40bdb9c4ccb17c6877de4b9115d1120cc0e4a40
maksimn <maksim1989sovunion@yandex.ru>
2021-11-11 16:29:32 +0300

    [TZ-4.5]: Удаление неактуальных тестов.

--- PersonalDictionaryTests/NewWord/Graph/NewWordMVVMImplTests.swift
+++ PersonalDictionaryTests/NewWord/Graph/NewWordMVVMImplTests.swift
@@ -10,17 +10,4 @@ import XCTest
 
 class NewWordMVVMImplTests: XCTestCase {
 
-    func test_mvvmGraphOutputNotNil() throws {
-        let mockLangRepository = MockLangRepository()
-        let newWordMVVM = NewWordMVVMImpl(langRepository: mockLangRepository,
-                                          notificationCenter: NotificationCenter.default,
-                                          viewParams: NewWordViewParams(
-                                            staticContent: NewWordViewStaticContent(selectButtonTitle: "",
-                                                                                    arrowText: "",
-                                                                                    okText: "",
-                                                                                    textFieldPlaceholder: ""),
-                                            styles: NewWordViewStyles(backgroundColor: .clear)))
-
-        XCTAssertNotNil(newWordMVVM.viewController)
-    }
 }
