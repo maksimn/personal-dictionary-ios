d671cabbc99478b18d0f1a262cb687f4a5acb4a9
maksimn <maksim1989sovunion@yandex.ru>
2021-11-12 14:25:26 +0300

    [TZ-4.5]: SearchViewController searchBar has been removed.

--- PersonalDictionary/UI/BigFeature/Search/ViewController/SearchViewController.swift
+++ PersonalDictionary/UI/BigFeature/Search/ViewController/SearchViewController.swift
@@ -9,20 +9,8 @@ import UIKit
 
 final class SearchViewController: UIViewController {
 
-    let searchBar = UISearchBar()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
-        initSearchBar()
-    }
-}
-
-extension SearchViewController: UISearchBarDelegate {
-
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if let searchText = searchBar.text?.trimmingCharacters(in: .whitespacesAndNewlines) {
-            print(searchText)
-        }
     }
 }
--- PersonalDictionary/UI/Feature/SearchTextInput/View/SearchTextInputViewImpl.swift
+++ PersonalDictionary/UI/Feature/SearchTextInput/View/SearchTextInputViewImpl.swift
@@ -7,7 +7,25 @@
 
 import UIKit
 
-final class SearchTextInputViewImpl: UIView, SearchTextInputView {
+final class SearchTextInputViewImpl: UIView, SearchTextInputView, UISearchBarDelegate {
 
     var viewModel: SearchTextInputViewModel?
+
+    let searchBar = UISearchBar()
+
+    func initSearchBar() {
+        let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 72, height: 44)
+
+        searchBar.isUserInteractionEnabled = true
+        searchBar.frame = frame
+        searchBar.placeholder = NSLocalizedString("Enter a new word", comment: "")
+        searchBar.becomeFirstResponder()
+        searchBar.delegate = self
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if let searchText = searchBar.text?.trimmingCharacters(in: .whitespacesAndNewlines) {
+            print(searchText)
+        }
+    }
 }
