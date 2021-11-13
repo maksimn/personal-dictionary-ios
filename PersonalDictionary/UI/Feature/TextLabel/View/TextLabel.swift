92d4d00527a8dfce575f51bb90c0d4d3d7bb1f65
maksimn <maksim1989sovunion@yandex.ru>
2021-11-13 04:06:24 +0300

    [TZ-4.5]: TextLabel impl.

--- PersonalDictionary/UI/Feature/TextLabel/View/TextLabel.swift
+++ PersonalDictionary/UI/Feature/TextLabel/View/TextLabel.swift
@@ -9,4 +9,27 @@ import UIKit
 
 final class TextLabel: UIView {
 
+    private let label = UILabel()
+
+    init() {
+        super.init(frame: .zero)
+        initWordsNotFoundLabelLabel()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func initWordsNotFoundLabelLabel() {
+        label.textColor = .darkGray
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.numberOfLines = 1
+        label.textAlignment = .center
+        label.text = NSLocalizedString("No words found", comment: "")
+        label.isHidden = true
+        addSubview(label)
+        label.snp.makeConstraints { make -> Void in
+            make.edges.equalTo(self)
+        }
+    }
 }
