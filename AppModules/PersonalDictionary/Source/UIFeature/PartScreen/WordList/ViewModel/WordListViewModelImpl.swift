c280a712b675906e5e01fd2202d0f75957b3c513
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2023-03-09 20:59:08 +0300

    [TZ-45]: WordListViewModel refactored.

--- AppModules/PersonalDictionary/Source/UIFeature/PartScreen/WordList/ViewModel/WordListViewModelImpl.swift
+++ AppModules/PersonalDictionary/Source/UIFeature/PartScreen/WordList/ViewModel/WordListViewModelImpl.swift
@@ -26,10 +26,7 @@ final class WordListViewModelImpl: WordListViewModel {
     }
 
     func remove(at position: Int) {
-        guard position > -1 && position < wordList.value.count else { return }
-        let word = wordList.value[position]
-
-        remove(word, at: position, withSideEffect: true)
+        remove(at: position, withSideEffect: true)
     }
 
     func toggleWordIsFavorite(at position: Int) {
@@ -44,28 +41,28 @@ final class WordListViewModelImpl: WordListViewModel {
         model.fetchTranslationsFor(state: wordList.value, start: start, end: end)
             .executeInBackgroundAndObserveOnMainThread()
             .map { [weak self] state in
-                self?.logState(action: "fetchTranslationsIfNeeded", state)
-                self?.wordList.accept(state)
-
+                self?.onNewState(state, actionName: "fetchTranslationsIfNeeded")
                 return state
             }
             .asCompletable()
     }
 
+    private func onNewState(_ state: WordListState, actionName: String) {
+        logState(actionName: actionName, state)
+
+        wordList.accept(state)
+    }
+
     private func create(_ word: Word) {
         let wordList = model.create(word, state: self.wordList.value)
 
-        logState(action: "create word", wordList)
-
-        self.wordList.accept(wordList)
+        onNewState(wordList, actionName: "create word")
 
         model.createEffect(word, state: wordList)
             .executeInBackgroundAndObserveOnMainThread()
             .subscribe(
                 onSuccess: { [weak self] wordList in
-                    self?.logState(action: "create effect", wordList)
-
-                    self?.wordList.accept(wordList)
+                    self?.onNewState(wordList, actionName: "create effect")
                 },
                 onError: { [weak self] error in
                     self?.logger.logError(error, source: "create word effect")
@@ -76,9 +73,7 @@ final class WordListViewModelImpl: WordListViewModel {
     private func update(_ word: Word, at position: Int, withSideEffect: Bool) {
         let wordList = model.update(word, at: position, state: self.wordList.value)
 
-        logState(action: "update word", wordList)
-
-        self.wordList.accept(wordList)
+        onNewState(wordList, actionName: "update word")
 
         guard withSideEffect else { return }
 
@@ -91,12 +86,12 @@ final class WordListViewModelImpl: WordListViewModel {
             ).disposed(by: disposeBag)
     }
 
-    private func remove(_ word: Word, at position: Int, withSideEffect: Bool) {
+    private func remove(at position: Int, withSideEffect: Bool) {
+        guard position > -1 && position < wordList.value.count else { return }
+        let word = wordList.value[position]
         let wordList = model.remove(at: position, state: self.wordList.value)
 
-        logState(action: "remove word", wordList)
-
-        self.wordList.accept(wordList)
+        onNewState(wordList, actionName: "remove word")
 
         guard withSideEffect else { return }
 
@@ -112,7 +107,7 @@ final class WordListViewModelImpl: WordListViewModel {
     private func findAndRemove(_ word: Word) {
         guard let position = wordList.value.firstIndex(where: { $0.id == word.id }) else { return }
 
-        remove(word, at: position, withSideEffect: false)
+        remove(at: position, withSideEffect: false)
     }
 
     private func findAndUpdate(_ word: Word) {
@@ -124,21 +119,21 @@ final class WordListViewModelImpl: WordListViewModel {
     private func subscribe() {
         wordStream.newWord
             .subscribe(onNext: { [weak self] in
-                self?.log($0, from: "new word")
+                self?.log($0, fromModelStream: "new word")
 
                 self?.create($0)
             })
             .disposed(by: disposeBag)
         wordStream.removedWord
             .subscribe(onNext: { [weak self] in
-                self?.log($0, from: "removed word")
+                self?.log($0, fromModelStream: "removed word")
 
                 self?.findAndRemove($0)
             })
             .disposed(by: disposeBag)
         wordStream.updatedWord
             .subscribe(onNext: { [weak self] in
-                self?.log($0, from: "updated word")
+                self?.log($0, fromModelStream: "updated word")
 
                 self?.findAndUpdate($0)
             })
@@ -147,8 +142,8 @@ final class WordListViewModelImpl: WordListViewModel {
 
     // MARK: - Logging
 
-    private func logState(action: String, _ state: WordListState) {
-        logger.log("Word list \(action) result:")
+    private func logState(actionName: String, _ state: WordListState) {
+        logger.log("Word list \(actionName) result:")
 
         if wordList.value == state {
             logger.log("\t\tno state changes")
@@ -157,7 +152,7 @@ final class WordListViewModelImpl: WordListViewModel {
         }
     }
 
-    private func log(_ word: Word, from modelStreamName: String) {
+    private func log(_ word: Word, fromModelStream modelStreamName: String) {
         logger.log("Received word = \(word) from the \(modelStreamName) model stream.")
     }
 }
