bfe2585c6d5c2651f2f65d53836b08688248c64a
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2023-07-05 12:27:42 +0300

    [TZ-99]: App.swift polishing.

--- AppModules/TodoList/Source/App/UDF/App.swift
+++ AppModules/TodoList/Source/App/UDF/App.swift
@@ -21,34 +21,11 @@ struct App: ReducerProtocol {
     }
 
     enum Action {
-        case cachedTodosLoaded(todos: [Todo])
-
-        case getRemoteTodosResult(TaskResult<[Todo]>)
-
-        case replaceAllTodosInCache([Todo])
-        case replaceAllTodosInCacheResult(TaskResult<[Todo]>)
-
         case syncWithRemoteTodos
-        case syncWithRemoteTodosResult(TaskResult<[Todo]>)
-
-        case todo(TodoAction)
-
+        case update(Todo)
+        case updateTodos([Todo])
         case mainList(MainList.Action)
         case networkIndicator(NetworkIndicator.Action)
-    }
-
-    enum TodoAction {
-        case create(Todo)
-        case createRemote(Todo)
-        case createRemoteResult(TaskResult<Todo>)
-
-        case update(Todo)
-        case updateRemote(Todo)
-        case updateRemoteResult(TaskResult<Todo>)
-
-        case delete(Todo)
-        case deleteRemote(Todo)
-        case deleteRemoteResult(TaskResult<Todo>)
     }
 
     var body: some ReducerProtocol<State, Action> {
@@ -65,54 +42,22 @@ struct App: ReducerProtocol {
 
     private func reduceInto(_ state: inout State, action: Action) -> EffectTask<Action> {
         switch action {
-        case .cachedTodosLoaded(let todos):
-            update(state: &state, with: todos)
-
-        case .getRemoteTodosResult(.success(let todos)),
-             .syncWithRemoteTodosResult(.success(let todos)):
-            update(state: &state, with: todos)
-
-            return .run { send in
-                await send(.networkIndicator(.decrementNetworkRequestCount))
-                await send(.replaceAllTodosInCache(todos))
-            }
-
-        case .replaceAllTodosInCache(let todos):
-            return .run { send in
-                await send(
-                    .replaceAllTodosInCacheResult(
-                        TaskResult {
-                            try await cache.replaceWith(todos)
-                            return todos
-                        }
-                    )
-                )
-            }
-
-        case .getRemoteTodosResult(.failure):
-            return .send(.networkIndicator(.decrementNetworkRequestCount))
+        case .updateTodos(let todos):
+            state.mainList.todos = todos
+            state.mainList.completedTodoCount = todos.filter({ $0.isCompleted }).count
+            return .none
 
         case .syncWithRemoteTodos:
-            let deleted = Array(Set(deadCache.items.map { $0.todoId }))
-            let dirtyTodos = cache.todos.filter { $0.isDirty }
+            return make(serviceOp: {
+                let deleted = Array(Set(deadCache.items.map { $0.todoId }))
+                let dirtyTodos = cache.todos.filter { $0.isDirty }
+                let todos = try await service.syncWithRemote(deleted, dirtyTodos)
+                try await deadCache.clear()
+                return todos
+            })
 
-            return .run { send in
-                await send(.networkIndicator(.incrementNetworkRequestCount))
-
-                do {
-                    let todos = try await service.syncWithRemote(deleted, dirtyTodos)
-
-                    try await deadCache.clear()
-                    await send(.syncWithRemoteTodosResult(.success(todos)))
-                } catch {
-                    await send(.syncWithRemoteTodosResult(.failure(error)))
-                }
-
-                await send(.networkIndicator(.decrementNetworkRequestCount))
-            }
-
-        case .todo(let action):
-            return reduceInto(&state, todoAction: action)
+        case .update(let todo):
+            return update(todo)
 
         case .mainList(let action):
             return reduceInto(&state, mainListAction: action)
@@ -120,162 +65,119 @@ struct App: ReducerProtocol {
         default:
             return .none
         }
-
-        return .none
     }
 
     private func reduceInto(_ state: inout State, mainListAction action: MainList.Action) -> EffectTask<Action> {
         switch action {
         case .loadCachedTodos:
-            return .send(.cachedTodosLoaded(todos: cache.todos))
+            return .send(.updateTodos(cache.todos))
 
         case .getRemoteTodos:
             if cache.isDirty {
                 return .send(.syncWithRemoteTodos)
             }
 
-            return .run { send in
-                await send(.networkIndicator(.incrementNetworkRequestCount))
-                await send(.getRemoteTodosResult(TaskResult { try await service.getTodos() }))
-            }
+            return make(serviceOp: { try await service.getTodos() })
 
         case .createTodo(let todo):
-            return .send(.todo(.create(todo)))
+            return create(todo)
 
         case .toggleTodoCompletion(let todo):
-            return .send(.todo(.update(todo.update(isCompleted: !todo.isCompleted))))
+            return .send(.update(todo.update(isCompleted: !todo.isCompleted)))
 
         case .deleteTodo(let todo):
-            return .send(.todo(.delete(todo)))
+            return delete(todo)
 
         case .editor(.saveTodo(let todo, let mode)):
             return editor(save: todo, mode: mode)
 
         case .editor(.deleteTodo(let todo)):
-            return .send(.todo(.delete(todo)))
+            return delete(todo)
 
         default:
             return .none
         }
     }
 
-    private func update(state: inout State, with todos: [Todo]) {
-        state.mainList.todos = todos
-        state.mainList.completedTodoCount = todos.filter({ $0.isCompleted }).count
-    }
-
     private func editor(save todo: Todo, mode: EditorMode) -> EffectTask<Action> {
         switch mode {
         case .create:
-            return .send(.todo(.create(todo)))
-
-        case .edit:
-            return .send(.todo(.update(todo)))
-        }
-    }
-
-    private func reduceInto(_ state: inout State, todoAction action: TodoAction) -> EffectTask<Action> {
-        switch action {
-        case .create(let todo):
             return create(todo)
 
-        case .createRemote(let todo):
-            return .run { send in
-                await send(.networkIndicator(.incrementNetworkRequestCount))
-                await send(.todo(.createRemoteResult(
-                    TaskResult {
-                        try await service.createRemote(todo)
-                        return todo
-                    }
-                )))
-            }
-
-        case .createRemoteResult(.success(let todo)),
-             .updateRemoteResult(.success(let todo)):
-            return .run { send in
-                await send(.networkIndicator(.decrementNetworkRequestCount))
-                try await cache.update(todo.update(isDirty: false))
-            }
-
-        case .createRemoteResult(.failure),
-             .updateRemoteResult(.failure),
-             .deleteRemoteResult:
-            return .send(.networkIndicator(.decrementNetworkRequestCount))
-
-        case .update(let todo):
+        case .edit:
             return update(todo)
-
-        case .updateRemote(let todo):
-            return .run { send in
-                await send(.networkIndicator(.incrementNetworkRequestCount))
-                await send(.todo(.updateRemoteResult(
-                    TaskResult {
-                        try await service.updateRemote(todo)
-                        return todo
-                    }
-                )))
-            }
-
-        case .delete(let todo):
-            return .run { send in
-                try await cache.delete(todo)
-                await send(.todo(.deleteRemote(todo)))
-            }
-
-        case .deleteRemote(let todo):
-            return deleteRemote(todo)
         }
     }
 
     private func create(_ todo: Todo) -> EffectTask<Action> {
+        return make(todo, dbOp: { try await cache.insert($0) }, serviceOp: { try await service.createRemote($0) })
+    }
+
+    private func update(_ todo: Todo) -> EffectTask<Action> {
+        return make(todo, dbOp: { try await cache.update($0) }, serviceOp: { try await service.updateRemote($0) })
+    }
+
+    private func delete(_ todo: Todo) -> EffectTask<Action> {
+        return .run { send in
+            try await cache.delete(todo)
+
+            if cache.isDirty {
+                try await deadCache.insert(Tombstone(todoId: todo.id, deletedAt: currentDate()))
+                return await send(.syncWithRemoteTodos)
+            }
+
+            await send(.networkIndicator(.incrementNetworkRequestCount))
+
+            do {
+                try await service.deleteRemote(todo)
+                await send(.networkIndicator(.decrementNetworkRequestCount))
+            } catch {
+                await send(.networkIndicator(.decrementNetworkRequestCount))
+                try await deadCache.insert(Tombstone(todoId: todo.id, deletedAt: currentDate()))
+                throw error
+            }
+        }
+    }
+
+    private func make(serviceOp: @escaping () async throws -> [Todo]) -> EffectTask<Action> {
+        return .run { send in
+            do {
+                await send(.networkIndicator(.incrementNetworkRequestCount))
+                let todos = try await serviceOp()
+
+                await send(.updateTodos(todos))
+                try await cache.replaceWith(todos)
+                await send(.networkIndicator(.decrementNetworkRequestCount))
+            } catch {
+                await send(.networkIndicator(.decrementNetworkRequestCount))
+                throw error
+            }
+        }
+    }
+
+    private func make(_ todo: Todo, dbOp: @escaping (Todo) async throws -> Void,
+                      serviceOp: @escaping (Todo) async throws -> Void) -> EffectTask<Action> {
         let dirtyTodo = todo.update(isDirty: true)
         let cleanTodo = todo.update(isDirty: false)
 
         if cache.isDirty {
             return .run { send in
-                try await cache.insert(dirtyTodo)
+                try await dbOp(dirtyTodo)
                 await send(.syncWithRemoteTodos)
             }
         }
 
         return .run { send in
-            try await cache.insert(dirtyTodo)
-            await send(.todo(.createRemote(cleanTodo)))
-        }
-    }
-
-    private func update(_ todo: Todo) -> EffectTask<Action> {
-        let dirtyTodo = todo.update(isDirty: true)
-
-        if cache.isDirty {
-            return .run { send in
-                try await cache.update(dirtyTodo)
-                await send(.syncWithRemoteTodos)
-            }
-        }
-
-        return .run { send in
-            try await cache.update(dirtyTodo)
-            await send(.todo(.updateRemote(dirtyTodo)))
-        }
-    }
-
-    private func deleteRemote(_ todo: Todo) -> EffectTask<Action> {
-        if cache.isDirty {
-            return .run { send in
-                try await deadCache.insert(Tombstone(todoId: todo.id, deletedAt: currentDate()))
-                await send(.syncWithRemoteTodos)
-            }
-        }
-
-        return .run { send in
+            try await dbOp(dirtyTodo)
             await send(.networkIndicator(.incrementNetworkRequestCount))
-            await send(.todo(.deleteRemoteResult(
-                TaskResult {
-                    try await service.deleteRemote(todo)
-                    return todo
-                }
-            )))
+            do {
+                try await serviceOp(cleanTodo)
+                try await cache.update(cleanTodo)
+                await send(.networkIndicator(.decrementNetworkRequestCount))
+            } catch {
+                await send(.networkIndicator(.decrementNetworkRequestCount))
+                throw error
+            }
         }
     }
 }
